dab5892c04f9bcb8ea645b2cdc3c5baaad6f5a07
weerut panjawattana <weerut.pan@gmail.com>
2017-04-11 18:58:30 +0700

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { Router } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Main App
+import { AppComponent }            from './app.component';
+// Main Module
+import { AppRoutingModule }        from './app-routing.module';
+// Component Module
+import { HeroesModule }            from './heroes/heroes.module';
+import { LoginRoutingModule }      from './login-routing.module';
+// Child Component
+import { ComposeMessageComponent } from './compose-message.component/compose-message.component';
+import { LoginComponent }          from './login.component';
+import { PageNotFoundComponent }   from './not-found.component';
+// Service
+import { DialogService }           from './dialog.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HeroesModule,
+    LoginRoutingModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ComposeMessageComponent,
+    LoginComponent,
+    PageNotFoundComponent
+  ],
+  providers: [
+    DialogService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+}
